fdecf9f0245104c55f338e33aee54ed10b5a959f
Richard阿辰 <1943126663@qq.com>
2023-11-05 22:51:52 +0800

    Update

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,18 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from "react-chat-engine-pretty"
+
+
+const ChatsPage = (props) => {
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow
+                projectId="ff872119-f505-4217-9433-f0c09f29274a"
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
+        </div>
+
+    )
 }
 export default ChatsPage
