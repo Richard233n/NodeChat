f958c8a6d359552138823a0865fa342ed0fb05b2
Richard阿辰 <1943126663@qq.com>
2023-11-05 19:47:13 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+    const { username } = req.body;
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "d0c8eb6a-50ce-4ced-8bfe-74fb143012a3 " } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return e.respose.status(r.status).json(e.respose.data)
+    }
 });
 
 app.listen(3001);
